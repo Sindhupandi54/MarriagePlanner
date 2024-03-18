1b0c87b9edfee4c699ba497a92837cbb2914b14a
Nishithagoudp <144953399+Nishithagoudp@users.noreply.github.com>
2024-03-18 10:25:09 +0530

    UpdateD AdminDashboard

--- marriage/src/Admin/AdminDashboard/AdminDashboard.jsx
+++ marriage/src/Admin/AdminDashboard/AdminDashboard.jsx
@@ -2,34 +2,36 @@ import React from "react";
 import "./AdminDashboard.css";
 import AdminNavbar from "../AdminNavbar/AdminNavbar";
 import NumberAnimator from "../../Components/NumberAnimator";
+
 export default function AdminDashboard() {
-    const DashboardItem = (name,value) =>{
-        return <>
-        <div className="admin-dashboard-item">
-                    <div className="admin-dashboard-item-title">
-                        <span>{name}</span>
-                        <div className="Numbers-count">
+    const DashboardItem = (name, value) => {
+        return (
+            <div className="admin-dashboard-item">
+                <div className="admin-dashboard-item-title">
+                    <span>{name}</span>
+                    <div className="Numbers-count">
                         <NumberAnimator counter={value}/>
-                        </div>
                     </div>
                 </div>
-        </>
-    }
+            </div>
+        );
+    };
+
     return (
         <>
             <AdminNavbar />
             <div className="admin-dashboard">
-                {DashboardItem('Decorers',10)}
-                {DashboardItem('Venuers',20)}
-                {DashboardItem('Caterers',30)}
-                {DashboardItem('Total ',40)}
+                <React.Fragment key="Decorers">{DashboardItem('Decorers', 10)}</React.Fragment>
+                <React.Fragment key="Venuers">{DashboardItem('Venuers', 20)}</React.Fragment>
+                <React.Fragment key="Caterers">{DashboardItem('Caterers', 30)}</React.Fragment>
+                <React.Fragment key="Total">{DashboardItem('Total', 40)}</React.Fragment>
             </div>
 
             <div className="admin-dashboard">
-                {DashboardItem('Last Month Decorers Booked',10)}
-                {DashboardItem('Last Month Venuers Booked',20)}
-                {DashboardItem('Last Month Caterers Booked',30)}
-                {DashboardItem('Total Bookings',40)}
+                <React.Fragment key="LastMonthDecorers">{DashboardItem('Last Month Decorers Booked', 10)}</React.Fragment>
+                <React.Fragment key="LastMonthVenuers">{DashboardItem('Last Month Venuers Booked', 20)}</React.Fragment>
+                <React.Fragment key="LastMonthCaterers">{DashboardItem('Last Month Caterers Booked', 30)}</React.Fragment>
+                <React.Fragment key="TotalBookings">{DashboardItem('Total Bookings', 40)}</React.Fragment>
             </div>
         </>
     );
