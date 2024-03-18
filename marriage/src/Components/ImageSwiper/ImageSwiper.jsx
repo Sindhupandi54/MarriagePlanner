f03103f2b272140794c693c569d31d7ffaacdbe4
Nishithagoudp <144953399+Nishithagoudp@users.noreply.github.com>
2024-03-18 10:26:39 +0530

    UpdateD ImageSwiper

--- marriage/src/Components/ImageSwiper/ImageSwiper.jsx
+++ marriage/src/Components/ImageSwiper/ImageSwiper.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import "./ImageSwiper.css";
 
-class ImageSwiper extends Component {
-  render() {
-      return (
+const ImageSwiper = () => {
+    return (
         <div className="slider-container">
-          <Carousel className="carousel-style" autoPlay="true" infiniteLoop="true" interval="2000" showArrows={true} showThumbs={false} showStatus={false}>
-              <div className="slider-item-div">
-              <img className="carousel-image" src="assets/wedding-image1.jpg" alt="" />
-              </div>
-              <div className="slider-item-div">
-              <img className="carousel-image" src="assets/wedding-image2.jpg" alt="" />
-              </div>
-              <div className="slider-item-div">
-              <img className="carousel-image" src="assets/wedding-image3.jpg" alt="" />
-              </div>
-          </Carousel>
-          </div>
-      );
-  }
+            <Carousel className="carousel-style" autoPlay={true} infiniteLoop={true} interval={2000} showArrows={true} showThumbs={false} showStatus={false}>
+                <div className="slider-item-div">
+                    <img className="carousel-image" src="assets/wedding-image1.jpg" alt="" />
+                </div>
+                <div className="slider-item-div">
+                    <img className="carousel-image" src="assets/wedding-image2.jpg" alt="" />
+                </div>
+                <div className="slider-item-div">
+                    <img className="carousel-image" src="assets/wedding-image3.jpg" alt="" />
+                </div>
+            </Carousel>
+        </div>
+    );
 };
 
 export default ImageSwiper;
