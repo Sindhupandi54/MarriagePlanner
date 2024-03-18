8af98bffc5d55b9982656c60e8289d217ad09f0b
ManumithaK <146122057+ManumithaK@users.noreply.github.com>
2024-03-18 10:30:27 +0530

    Updated Dashboard

--- marriage/src/Pages/Dashboard/Dashboard.jsx
+++ marriage/src/Pages/Dashboard/Dashboard.jsx
@@ -7,144 +7,91 @@ import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
     const navigate = useNavigate();
-    const handleSeeMore=()=>{
-        navigate('/CategoryDetails');
-    }
+
+    const redirectToCategoryDetails = (categoryName) => {
+        let destinationPath = '';
+        switch (categoryName) {
+            case 'Venue':
+                destinationPath = '/venue';
+                break;
+            case 'Decor':
+                destinationPath = '/decorer';
+                break;
+            case 'Caterer':
+                destinationPath = '/caterer';
+                break;
+            default:
+                destinationPath = '/';
+                break;
+        }
+        navigate(destinationPath);
+    };
+
+    const handleSeeMore = (categoryName) => {
+        redirectToCategoryDetails(categoryName);
+    };
+
+    const renderCategory = (categoryName) => {
+        return (
+            <div className="Dashboard-content">
+                <div className="category">
+                    <div className="category-name" onClick={() => redirectToCategoryDetails(categoryName)}>
+                        {categoryName}
+                    </div>
+                </div>
+                <div className="grid-container">
+                    <div className="grid-item">
+                        <div className="grid-item-details">
+                            <div>
+                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" />
+                            </div>
+                            <div className="grid-item-details-text">
+                                <div>{categoryName} Name</div>
+                                <div className="grid-item-details-text-area">{categoryName} Area Details</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid-item">
+                        <div className="grid-item-details">
+                            <div>
+                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" />
+                            </div>
+                            <div className="grid-item-details-text">
+                                <div>{categoryName} Name</div>
+                                <div className="grid-item-details-text-area">{categoryName} Area Details</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid-item">
+                        <div className="grid-item-details">
+                            <div>
+                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" />
+                            </div>
+                            <div className="grid-item-details-text">
+                                <div>{categoryName} Name</div>
+                                <div className="grid-item-details-text-area">{categoryName} Area Details</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid-item small">
+                        <button className="more-button" onClick={() => handleSeeMore(categoryName)}>
+                            <FontAwesomeIcon className="more-button-icon" icon={faChevronCircleDown} />See More
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
     return (
         <>
             <Navbar/>
             <div className="dashboard-outer-container">
-            <div className="Dashboard-content">
-                <div className="category">
-                    <div className="category-name">
-                        Venue
-                    </div>
-                </div>
-                <div className="grid-container">
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item small">
-                        <button className="more-button" onClick={handleSeeMore}>
-                            <FontAwesomeIcon className="more-button-icon" icon={faChevronCircleDown} />See More</button>
-                    </div>
-                </div>
-            </div>
-            <div className="Dashboard-content">
-                <div className="category">
-                    <div className="category-name">
-                        Venue
-                    </div>
-                </div>
-                <div className="grid-container">
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item small">
-                        <button className="more-button">
-                            <FontAwesomeIcon className="more-button-icon" icon={faChevronCircleDown} />See More</button>
-                    </div>
-                </div>
-            </div>
-            <div className="Dashboard-content">
-                <div className="category">
-                    <div className="category-name">
-                        Venue
-                    </div>
-                </div>
-                <div className="grid-container">
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item">
-                        <div className="grid-item-details">
-                            <div>
-                                <img className="grid-item-details-img" src="assets/wedding-image1.jpg" alt="" /></div>
-                            <div className="grid-item-details-text">
-                                <div>Venue Name</div>
-                                <div className="grid-item-details-text-area">Venue Area Details</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="grid-item small">
-                        <button className="more-button">
-                            <FontAwesomeIcon className="more-button-icon" icon={faChevronCircleDown} />See More</button>
-                    </div>
-                </div>
-            </div>
+                {renderCategory("Venue")}
+                {renderCategory("Decor")}
+                {renderCategory("Caterer")}
+                {/* Add more categories as needed */}
             </div>
         </>
-    )
-
+    );
 }
